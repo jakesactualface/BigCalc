9bb101f052a85e9c177f12b648d3edf508c904e8
Jake Everhart <jake.a.everhart@gmail.com>
2017-12-12 13:31:32 -0600

    Placeholder extension for arithmetic string addition.

---
+++ BigCalc/BigCalc/StringArithmetic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BigCalc
+{
+    public static class StringArithmetic
+    {
+        public static string Addition(this string lhs, string rhs)
+        {
+            // TODO handle parsing
+
+            return lhs + " + " + rhs; // TODO implement addition here
+        }
+    }
+}
