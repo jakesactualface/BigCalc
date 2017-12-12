2ff7dea340f88eca470828203882ef045895fca3
Jake Everhart <jake.a.everhart@gmail.com>
2017-12-12 16:09:09 -0600

    Added tests for negative addition (failing, not yet implemented).

--- BigCalc/StringArithmeticTest/AdditionTest.cs
+++ BigCalc/StringArithmeticTest/AdditionTest.cs
@@ -7,7 +7,7 @@ namespace StringArithmeticTest
     public class AdditionTest
     {
         [TestMethod]
-        public void ZeroAdditionTrimTest_Success()
+        public void ZeroTrimTest_Success()
         {
             const string lhs = "0000000000000";
             const string rhs = "0000000000000";
@@ -20,7 +20,7 @@ namespace StringArithmeticTest
         }
 
         [TestMethod]
-        public void ZeroAdditionLhsTrimTest_Success()
+        public void ZeroLhsTrimTest_Success()
         {
             const string lhs = "0000000000000";
             const string rhs = "0";
@@ -33,7 +33,7 @@ namespace StringArithmeticTest
         }
 
         [TestMethod]
-        public void ZeroAdditionRhsTrimTest_Success()
+        public void ZeroRhsTrimTest_Success()
         {
             const string lhs = "0";
             const string rhs = "0000000000000";
@@ -46,7 +46,7 @@ namespace StringArithmeticTest
         }
 
         [TestMethod]
-        public void ZeroAdditionSmallTest_Success()
+        public void ZeroSmallTest_Success()
         {
             const string lhs = "0";
             const string rhs = "0";
@@ -59,7 +59,7 @@ namespace StringArithmeticTest
         }
 
         [TestMethod]
-        public void SmallAdditionTest_Success()
+        public void SmallTest_Success()
         {
             const string lhs = "2";
             const string rhs = "3";
@@ -98,7 +98,7 @@ namespace StringArithmeticTest
         }
 
         [TestMethod]
-        public void LargeAdditionTest_Success()
+        public void LargeTest_Success()
         {
             const string lhs = "9999999999999999999999999999999999999999";
             const string rhs = "9999999999999999999999999999999999999999";
@@ -116,6 +116,136 @@ namespace StringArithmeticTest
             const string lhs = "11111q11111";
             const string rhs = "1111111111";
             const string correctResult = "2222222222";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void PollutedRhsTest_Success()
+        {
+            const string lhs = "1111111111";
+            const string rhs = "11111r11111";
+            const string correctResult = "2222222222";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void PollutedBothTest_Success()
+        {
+            const string lhs = "11111Q11111";
+            const string rhs = "11111R11111";
+            const string correctResult = "2222222222";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void NegativeLhsPositiveResultTest_Success()
+        {
+            const string lhs = "-2";
+            const string rhs = "3";
+            const string correctResult = "1";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void NegativeRhsPositiveResultTest_Success()
+        {
+            const string lhs = "3";
+            const string rhs = "-1";
+            const string correctResult = "2";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void NegativeLhsNegativeResultTest_Success()
+        {
+            const string lhs = "-3";
+            const string rhs = "2";
+            const string correctResult = "-1";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void NegativeRhsNegativeResultTest_Success()
+        {
+            const string lhs = "-3";
+            const string rhs = "1";
+            const string correctResult = "-2";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void NegativeLhsZeroResultTest_Success()
+        {
+            const string lhs = "-3";
+            const string rhs = "3";
+            const string correctResult = "0";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void NegativeRhsZeroResultTest_Success()
+        {
+            const string lhs = "-1";
+            const string rhs = "1";
+            const string correctResult = "0";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void NegativeSmallTest_Success()
+        {
+            const string lhs = "-3";
+            const string rhs = "-2";
+            const string correctResult = "-5";
+
+            var result = lhs.Addition(rhs);
+
+            Assert.AreEqual(correctResult, result,
+                $"Expected for {lhs} + {rhs} : {correctResult}; Actual: {result}");
+        }
+
+        [TestMethod]
+        public void NegativeLargeTest_Success()
+        {
+            const string lhs = "-9999999999999999999999999999999999999999";
+            const string rhs = "-9999999999999999999999999999999999999999";
+            const string correctResult = "-19999999999999999999999999999999999999998";
 
             var result = lhs.Addition(rhs);
 
