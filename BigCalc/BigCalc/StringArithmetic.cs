bd15a0881d65d9f051af7f60175f49ba4c9733f6
Jake Everhart <jake.a.everhart@gmail.com>
2017-12-12 13:38:55 -0600

    Initial implementation of string arithmetic addition.

--- BigCalc/BigCalc/StringArithmetic.cs
+++ BigCalc/BigCalc/StringArithmetic.cs
@@ -8,7 +8,46 @@ namespace BigCalc
         {
             // TODO handle parsing
 
-            return lhs + " + " + rhs; // TODO implement addition here
+            var result = new System.Text.StringBuilder();
+            var inputLength = 0;
+            var carry = 0;
+
+            inputLength = System.Math.Max(lhs.Length, rhs.Length);
+
+            if (lhs.Length > rhs.Length)
+            {
+                rhs = rhs.PadLeft(inputLength, '0');
+            }
+            else
+            {
+                lhs = lhs.PadLeft(inputLength, '0');
+            }
+
+            while (inputLength > 0)
+            {
+                var newDigit = ((int) lhs[inputLength - 1] - (int) '0') + ((int) rhs[inputLength - 1] - (int) '0') +
+                               carry;
+
+                if (newDigit > 9)
+                {
+                    carry = newDigit / 10;
+                    newDigit %= 10;
+                }
+                else
+                {
+                    carry = 0;
+                }
+
+                result.Insert(0, newDigit.ToString());
+                inputLength--;
+            }
+
+            if (carry > 0)
+            {
+                result.Insert(0, carry.ToString());
+            }
+
+            return result.ToString();
         }
     }
 }
